621a23e3defaf3e8d4ca7cd3e3f06c9a3973bbda
Peter Dietrich <xosofox@gmail.com>
2023-03-27 23:34:18 +0200

    UPDATE and npm cleanup

--- webpack.common.js
+++ webpack.common.js
@@ -1,8 +1,6 @@
 const path = require('path');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const webpack = require('webpack');
-const ExtractCssChunks = require('extract-css-chunks-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -20,34 +18,12 @@ module.exports = {
             automaticNameDelimiter: '-',
             chunks: 'all',
         },
-        minimizer: [
-            new OptimizeCSSAssetsPlugin({
-                cssProcessorPluginOptions: {
-                    preset: ['default', {discardComments: {removeAll: true}}],
-                },
-            }),
-        ],
     },
     plugins: [
         new CleanWebpackPlugin({cleanStaleWebpackAssets: true}),
-        new ExtractCssChunks({
-            // Options similar to the same options in webpackOptions.output
-            // both options are optional
-            filename: '[name].css?v=[contenthash:6]',
-            chunkFilename: '[id].css?v=[contenthash:6]',
-        }),
     ],
     module: {
         rules: [
-            {
-                test: /\.less$/,
-                use: [
-                    {loader: ExtractCssChunks.loader},
-                    {loader: 'css-loader', options: {url: false}},  // die logo files und svgs erst mal nicht checken
-                    {loader: "postcss-loader"},
-                    'less-loader',
-                ],
-            },
             {
                 test: /\.(png|svg|jpg|gif)$/,
                 use: [
